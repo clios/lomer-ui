577941e72738af6cc025a134171fe9020c03f5f6
Cliemtor B. Fabros <clios1208@gmail.com>
2024-11-23 18:24:32 +0800

    Install basic ui dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,8 @@ import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
-	theme: {
-		extend: {}
-	},
-
-	plugins: []
+	theme: { extend: {} },
+	plugins: [],
+	safelist: ['dark'],
+	darkMode: ['class']
 } satisfies Config;
