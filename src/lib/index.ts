706d2d9e0181f4e2978e03b799e4f4413895a31b
Cliemtor B. Fabros <clios1208@gmail.com>
2024-11-23 19:21:23 +0800

    Add default export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,23 @@
-// Reexport your entry components here
+import Alert from './alert.svelte';
+import Button from './button.svelte';
+import Dialog from './dialog.svelte';
+import Drawer from './drawer.svelte';
+import Field from './field.svelte';
+import Form from './form.svelte';
+import InputCheck from './input-check.svelte';
+import InputRadio from './input-radio.svelte';
+import InputSelect from './input-select.svelte';
+import InputText from './input-text.svelte';
+
+export default {
+	Alert,
+	Button,
+	Dialog,
+	Drawer,
+	Field,
+	Form,
+	InputCheck,
+	InputRadio,
+	InputSelect,
+	InputText
+};
