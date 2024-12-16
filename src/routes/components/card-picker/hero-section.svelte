482d107cbc91aae189a885c99dc0b623b751ee3f
Cliemtor B. Fabros <clios1208@gmail.com>
2024-12-17 07:28:31 +0800

    Add check mark in hero section card picker

--- src/routes/components/card-picker/hero-section.svelte
+++ src/routes/components/card-picker/hero-section.svelte
@@ -9,19 +9,19 @@
 <Screen class="p-8 text-center">
 	<p class="mb-4">Choose your clan</p>
 	<div class="mb-2 flex w-full justify-center gap-8">
-		<CardPicker bind:groupValue cardValue="dragon">
+		<CardPicker bind:groupValue cardValue="dragon" hasCheckMark>
 			<Icon icon="game-icons:spiked-dragon-head" width={100} height={100} />
 			<p>Dragon</p>
 		</CardPicker>
-		<CardPicker bind:groupValue cardValue="serpent">
+		<CardPicker bind:groupValue cardValue="serpent" hasCheckMark>
 			<Icon icon="game-icons:snake-tongue" width={100} height={100} />
 			<p>Serpent</p>
 		</CardPicker>
-		<CardPicker bind:groupValue cardValue="wolf">
+		<CardPicker bind:groupValue cardValue="wolf" hasCheckMark>
 			<Icon icon="game-icons:wolf-head" width={100} height={100} />
 			<p>Wolf</p>
 		</CardPicker>
-		<CardPicker bind:groupValue cardValue="lotus">
+		<CardPicker bind:groupValue cardValue="lotus" hasCheckMark>
 			<Icon icon="game-icons:lotus" width={100} height={100} />
 			<p>Lotus</p>
 		</CardPicker>
