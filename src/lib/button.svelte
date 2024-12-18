102a2a23bb78c731db18e0d6e34fb9589bf1ed8f
Cliemtor B. Fabros <clios1208@gmail.com>
2024-12-18 12:24:34 +0800

    Test button theme

--- src/lib/button.svelte
+++ src/lib/button.svelte
@@ -3,6 +3,13 @@
 	import type { Snippet } from 'svelte';
 	import { twMerge } from 'tailwind-merge';
 
+	const theme = {
+		light: 'zinc-50',
+		dark: 'zinc-950',
+		motif: 'cyan-500',
+		complimentary: 'red-500'
+	};
+
 	type Variant = 'primary' | 'secondary' | 'outline' | 'destructive' | 'ghost';
 
 	type Props = {
@@ -32,42 +39,42 @@
 <button
 	class={twMerge(
 		// BASE
-		'relative flex items-center gap-1', // layout and positioning
-		'rounded border border-cyan-500 px-3 py-1', // box model
-		'bg-zinc-950 dark:bg-zinc-50', // background
-		'text-zinc-50 dark:text-zinc-950', // text
-		'focus:outline focus:outline-1 focus:outline-offset-2 focus:outline-cyan-500', // focusing
-		'hover:outline hover:outline-1 hover:outline-offset-2 hover:outline-cyan-500', // hovering
-		'active:outline-2 active:outline-offset-0', // active
+		`relative flex items-center gap-1`, // layout and positioning
+		`rounded border border-${theme.motif} px-3 py-1`, // box model
+		`bg-${theme.dark} dark:bg-${theme.light}`, // background
+		`text-${theme.light} dark:text-${theme.dark}`, // text
+		`focus:outline focus:outline-1 focus:outline-offset-2 focus:outline-${theme.motif}`, // focusing
+		`hover:outline hover:outline-1 hover:outline-offset-2 hover:outline-${theme.motif}`, // hovering
+		`active:outline-2 active:outline-offset-0`, // active
 
 		// VARIANTS: SECONDARY
 		variant === 'secondary' && [
-			'text-zinc-950 dark:text-zinc-50', // text
-			'bg-zinc-50 dark:bg-zinc-900', // background
-			'border-zinc-200 dark:border-zinc-800' // border
+			`text-${theme.dark} dark:text-${theme.light}`, // text
+			`bg-${theme.light} dark:bg-zinc-900`, // background
+			`border-zinc-200 dark:border-zinc-800` // border
 		],
 
 		// VARIANTS: DESTRUCTIVE
 		variant === 'destructive' && [
-			'text-zinc-50 dark:text-red-50', // text
-			'bg-red-500 dark:bg-red-500', // background
-			'border-red-500 dark:border-red-500', // border
-			'focus:outline-red-500', // focusing
-			'hover:outline-red-500' // hovering
+			`text-${theme.light} dark:text-${theme.complimentary}`, // text
+			`bg-${theme.complimentary} dark:bg-${theme.complimentary}`, // background
+			`border-${theme.complimentary} dark:border-${theme.complimentary}`, // border
+			`focus:outline-${theme.complimentary}`, // focusing
+			`hover:outline-${theme.complimentary}` // hovering
 		],
 
 		// VARIANTS: GHOST
 		variant === 'ghost' && [
-			'text-zinc-950 dark:text-zinc-50', // text
-			'bg-zinc-950/0 dark:bg-zinc-50/0', // background
-			'border-zinc-500/0 dark:border-zinc-500/0' // border
+			`text-${theme.dark} dark:text-${theme.light}`, // text
+			`bg-${theme.dark}/0 dark:bg-${theme.light}/0`, // background
+			`border-zinc-500/0 dark:border-zinc-500/0` // border
 		],
 
 		// VARIANTS: OUTLINE
 		variant === 'outline' && [
-			'text-zinc-950 dark:text-zinc-50', // text
-			'bg-zinc-950/0 dark:bg-zinc-50/0', // background
-			'border-zinc-950 dark:border-zinc-50' // border
+			`text-${theme.dark} dark:text-${theme.light}`, // text
+			`bg-${theme.dark}/0 dark:bg-${theme.light}/0`, // background
+			`border-${theme.dark} dark:border-${theme.light}` // border
 		],
 
 		// SIZES
@@ -81,13 +88,13 @@
 		edge === 'sharp' && 'rounded-none',
 
 		// DISABLED
-		'disabled:border-zinc-700 disabled:dark:border-zinc-700', // border
-		'disabled:bg-zinc-700 disabled:dark:bg-zinc-700', // background
-		'disabled:text-zinc-400 disabled:dark:text-zinc-400', // text
-		'disabled:cursor-not-allowed disabled:outline-none', // visual
+		`disabled:border-zinc-700 disabled:dark:border-zinc-700`, // border
+		`disabled:bg-zinc-700 disabled:dark:bg-zinc-700`, // background
+		`disabled:text-zinc-400 disabled:dark:text-zinc-400`, // text
+		`disabled:cursor-not-allowed disabled:outline-none`, // visual
 
 		// LOADING
-		isLoading && 'disabled:text-zinc-500/0 disabled:dark:text-zinc-500/0',
+		isLoading && `disabled:text-zinc-500/0 disabled:dark:text-zinc-500/0`,
 
 		className
 	)}
@@ -103,11 +110,11 @@
 			viewBox="0 0 24 24"
 		>
 			<path
-				fill="#67E8F9"
+				fill={theme.motif}
 				d="M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z"
 				opacity="0.5"
 			/>
-			<path fill="#67E8F9" d="M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z">
+			<path fill={theme.motif} d="M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z">
 				<animateTransform
 					attributeName="transform"
 					dur="1s"
