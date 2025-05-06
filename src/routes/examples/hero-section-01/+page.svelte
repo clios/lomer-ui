c09aa1491296109696b1ca36d00b43cce3219fbf
Cliemtor B. Fabros <clios1208@gmail.com>
2025-05-06 14:40:27 +0800

    fix: missing route

--- src/routes/examples/hero-section-01/+page.svelte
+++ src/routes/examples/hero-section-01/+page.svelte
@@ -1,10 +1,5 @@
-<script>
-  import HeroSection_01 from '$lib/iframes/examples/hero-section-01.svelte';
-</script>
-
 <svelte:head>
   <title>Lomer UI - Hero Section 01</title>
 </svelte:head>
 
-<p>Example</p>
-<HeroSection_01 />
+<p>Example Hero Section 01</p>
